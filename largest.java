863899a28ff176be87f97082f73262f9e4cd5de8
rihan1228 <mohammedrihan7869@gmail.com>
2025-07-23 20:51:29 +0530

    modified

--- largest.java
+++ largest.java
@@ -1,10 +1,13 @@
 // write a java program to find largest number between two number
+import java.util.Scanner;
 class largest
 {
   public static void main(String[]args)
-  {
-    int num1=100;
-    int num2=200;
+  {Scanner sc =new Scanner(System.in);
+   System.out.println("enter first number");
+    int num1=sc.nextInt();
+    System.out.println("enter second number");
+    int num2=sc.nextInt();
     if(num1>num2)
     {
       System.out.println(num1+" is greater than "+num2);
