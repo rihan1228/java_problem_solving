ac921e923c038a6b2e55a2c766efa9c83f95aff0
rihan1228 <mohammedrihan7869@gmail.com>
2025-07-23 20:42:27 +0530

    basic problems

---
+++ largest.java
@@ -0,0 +1,17 @@
+// write a java program to find largest number between two number
+class largest
+{
+  public static void main(String[]args)
+  {
+    int num1=100;
+    int num2=200;
+    if(num1>num2)
+    {
+      System.out.println(num1+" is greater than "+num2);
+    }
+    else
+    {
+      System.out.println(num2+" is greater than  "+num1);
+    }
+  }
+}
---
+++ same_largest.java
@@ -0,0 +1,8 @@
+// write a java program to find largest number between two number
+class largest_same
+{
+  public static void main(String[]args)
+  {
+    int num1 
+  }
+}
