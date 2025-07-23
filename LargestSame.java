074e7ea43aca4fca1d7750a6f0dbd77d99b2eb8e
rihan1228 <mohammedrihan7869@gmail.com>
2025-07-23 21:03:23 +0530

    to find largest number between two numbers

---
+++ LargestSame.java
@@ -0,0 +1,25 @@
+// write a java program to find largest number between two number
+import java.util.Scanner;
+class LargestSame
+{
+  public static void main(String[]args)
+  {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter first number");
+    int num1=sc.nextInt();
+    System.out.println("enter second number");
+    int num2=sc.nextInt();
+    if (num1==num2)
+    {
+      System.out.println("entered two numbers are same ");
+    }
+    else if(num1>num2)
+    {
+       System.out.println(num1+" is greater than "+num2);
+    }
+    else
+    {
+      System.out.println(num1+" is greater than "+num2);
+    }
+  }
+}
--- same_largest.java
+++
@@ -1,8 +0,0 @@
-// write a java program to find largest number between two number
-class largest_same
-{
-  public static void main(String[]args)
-  {
-    int num1 
-  }
-}
